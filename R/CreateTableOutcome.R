1ffefb6a44460ee56f805ed156610cde47ea6521
Gautier <gautier@paux.fr>
2019-05-08 11:51:27 +0200

    fix outcome table generation for multivariate distribution with no correlation matrix

--- R/CreateTableOutcome.R
+++ R/CreateTableOutcome.R
@@ -20,8 +20,7 @@ CreateTableOutcome = function(data.structure, label = NULL) {
   outcome.dist.name = outcome.dist.desc[[2]]
 
   # Label
-  if (is.null(label)) label = paste0("Outcome ", 1:n.outcome)
-  else label = unlist(label)
+  if (is.null(label)) label = paste0("Outcome ", 1:n.outcome) else label = unlist(label)
   if (length(label) != n.outcome)
     stop("Summary: Number of the outcome parameters labels must be equal to the number of outcome parameters sets.")
 
@@ -43,12 +42,15 @@ CreateTableOutcome = function(data.structure, label = NULL) {
   if (data.structure$outcome$outcome.dist.dim > 1) {
     for (i in 1:n.outcome) {
       for (j in 1:n.id) {
-        par = paste0(mapply(function(x,y) paste0("{",mergeOutcomeParameter(x,y),"}"), parameter.labels$par, data.structure$outcome.parameter.set[[i]][[j]]$par), collapse = ", ")
-        corr = paste0("corr = {", paste(t(data.structure$outcome.parameter.set[[i]][[j]]$corr), collapse = ","),"}", collapse = "")
+        par = ifelse(!is.null(parameter.labels$par),
+                     paste0(mapply(function(x,y) paste0("{",mergeOutcomeParameter(x,y),"}"), parameter.labels$par, data.structure$outcome.parameter.set[[i]][[j]]$par), collapse = ", "),
+                     mergeOutcomeParameter(parameter.labels,  data.structure$outcome.parameter.set[[i]][[j]])
+        )
+        corr = ifelse(!is.null(data.structure$outcome.parameter.set[[i]][[j]]$corr), paste0("corr = {", paste(t(data.structure$outcome.parameter.set[[i]][[j]]$corr), collapse = ","),"}", collapse = ""), NA)
         outcome.table[ind, 1] = i
         outcome.table[ind, 2] = label[i]
         outcome.table[ind, 3] = id.label[j]
-        outcome.table[ind, 4] = paste0(par, ",\n",corr)
+        outcome.table[ind, 4] = ifelse(!is.na(corr), paste0(par, ",\n",corr),par)
         ind<-ind+1
       }
     }
